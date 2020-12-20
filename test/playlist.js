41c1562a015683ef5cbc6d420b268e2323a66fb5
Acie <913587892@qq.com>
2020-12-20 13:24:56 +0800

    Update playlist.js

--- test/playlist.js
+++ test/playlist.js
@@ -8,13 +8,17 @@ function init(){
 function handleButtonClick(){
   var textInput = document.getElementById("songTextInput");
   var songName = textInput.value;
-  // 希望创建的元素类型 "li"
-  var li = document.createElement("li"); 
-  // 把这个元素的内容设置为歌名
-  li.innerHTML = songName;
-  var ul = document.getElementById("playlist");
-  // 要求父元素ul增加li作为它的一个新孩子
-  ul.appendChild(li);
-  
+  if (songName == "") {
+		alert("Please enter a song");
+  }
+  else{
+    // 希望创建的元素类型 "li"
+    var li = document.createElement("li"); 
+    // 把这个元素的内容设置为歌名
+    li.innerHTML = songName;
+    var ul = document.getElementById("playlist");
+    // 要求父元素ul增加li作为它的一个新孩子
+    ul.appendChild(li);
+  }
 }
   
