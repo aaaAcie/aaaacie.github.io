230e24a4c0c30b07b80cb3aa0de820b80e68e802
Acie <913587892@qq.com>
2020-12-20 15:05:24 +0800

    Update playlist.js

--- test/playlist.js
+++ test/playlist.js
@@ -5,7 +5,7 @@ function init() {
   button.onclick = handleButtonClick;
 }
 
-function handleButtonClick(e) {
+function handleButtonClick() {
   var textInput = document.getElementById("songTextInput");
   var songName = textInput.value;
   if (songName == "") {
